00524cebfa287a86295cfb3aff8660ac23c73d9b
Daichi <daichisuzuki1018@gmail.com>
2026-06-30 12:19:12 -0700

    feat: add category search #14

--- frontend/src/components/WordFilter.tsx
+++ frontend/src/components/WordFilter.tsx
@@ -1,14 +1,21 @@
+import { on } from 'events';
+import { Category } from '../types/category'
+    ;
 type Filter = 'all' | 'learned' | 'unlearned';
 
-type  WordFilterProps = {
+type WordFilterProps = {
     search: string;
     setSearch: (search: string) => void;
     filter: Filter;
     onChange: (filter: Filter) => void;
+    categories: Category[];
+    categoryFilter: number | 'all';
+    onCategoryChange: (value: number | 'all') => void;
+
 }
 
 export default function WordFilter({
-    search, setSearch, filter, onChange }: WordFilterProps) {
+    search, setSearch, filter, onChange, categories, categoryFilter, onCategoryChange }: WordFilterProps) {
     return (
         <div className="m-4 flex justify-center mt-4">
             <input
@@ -21,7 +28,30 @@ export default function WordFilter({
 
             <button onClick={() => onChange('all')} className={`px-4 py-2 ${filter === 'all' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}>All</button>
             <button onClick={() => onChange('learned')} className={`px-4 py-2 ${filter === 'learned' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}>Learned</button>
-            <button onClick={() => onChange('unlearned')} className={`px-4 py-2 ${filter === 'unlearned' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}>Unlearned</button>   
-        </div>
+            <button onClick={() => onChange('unlearned')} className={`px-4 py-2 ${filter === 'unlearned' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}>Unlearned</button>
+            <select
+                className={`px-4 py-2 ${filter === 'unlearned' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+                value={categoryFilter}
+                onChange={(e) => {
+                    const value = e.target.value;
+
+                    onCategoryChange(
+                        value === 'all'
+                            ? 'all'
+                            : Number(value)
+                    );
+                }}
+            >
+            <option value='all'>All</option>
+            {categories.map(category => (
+                <option
+                    key={category.id}
+                    value={category.id}
+                >
+                    {category.name}
+                </option>
+            ))}
+        </select>
+        </div >
     )
 }
