5d1e50ed7cf155a67044bbe14f1b89c3adbf245c
Daichi <daichisuzuki1018@gmail.com>
2026-07-15 13:29:19 -0700

    feat:add create user #38

--- backend/app/Http/Controllers/Api/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Api/Auth/RegisterController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    public function register (RegisterRequest $request) {
+    public function store (RegisterRequest $request) {
         $validated = $request->validated();
+
+        $validated['password'] = Hash::make($validated['password']);
+
+        User::create($validated);
     }
 }
